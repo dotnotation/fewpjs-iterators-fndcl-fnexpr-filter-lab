9231bb912a9fb45a743ad0ef6a675c066f9c16ce
dotnotation <dotinmotionproductions@gmail.com>
2021-11-23 18:10:16 -0500

    commit tree 66bdf6ca9ddaa3e291e23217309e3bfb02aa9f84

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// all functions takes an array of drivers and a string as arguments
+
+function findMatching(array, string){
+    // returns the matching list of drivers
+    // should be case insensitive 
+    return array.filter(driver => driver.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(array, string){
+    // returns all drivers whose name begin with the provided letters
+    return array.filter(driver => driver.toUpperCase()[0] === string.toUpperCase()[0])
+}
+
+function matchName(array, string){
+    // each element of the array is an Object that has a property of name
+    // return each element whose name property matches the provided string argument
+    return array.filter(driver => driver.name === string)
+}
